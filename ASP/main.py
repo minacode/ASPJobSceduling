f5fdbf61faa542382d0a04050576f4fad6e43944
minacode <minamoto9@web.de>
2016-06-21 16:24:57 +0200

    zwischenstand. geht nicht

--- ASP/main.py
+++ ASP/main.py
@@ -13,36 +13,56 @@ class Operation:
     def get_dependency(self):
         return 'j' + str(self.job) + 'o' + str(self.rank -1)
         
-
-file = open('1.txt', 'r')
+file_name = input('file: ')
+file = open(file_name + '.txt', 'r')
 lines = file.readlines()
 counts = lines[0].split()
 del lines[0]
 operations = []
-for i in range(len(lines)):
+machine_count = len(lines)
+machines = []
+for i in range(machine_count):
+    machines.append("m"+ str(i))
     a = lines[i].split()
     for j in range(0, len(a), 2):
-        operations.append(Operation(job = i, machine = a[j], cost = a[j+1], rank = j // 2))
+        operations.append(Operation(job = i, machine = 'm' + a[j], cost = a[j+1], rank = j // 2))
 file.close()
 
 f = open('mysolution.lp', 'w')
+
+f.write('operation(pseudo).\n'
+        'starts(pseudo, m0, 0).\n'
+        'lasts(pseudo, 0).\n\n')
+
+max_sum = 0
 for operation in operations:
+    max_sum += int(operation.cost)
+    f.write('operation(' + operation.get_name() + ').\n')
     if operation.rank == 0:
         f.write('dependson(' + operation.get_name() + ', pseudo).\n')
     else:
         f.write('dependson(' + operation.get_name() + ', ' + operation.get_dependency() + ').\n')
     f.write('lasts(' + operation.get_name() + ', ' + operation.cost + ').\n')
-    f.write('runson(' + operation.get_name() + ', ' + operation.machine + ').\n')
+    f.write('runson(' + operation.get_name() + ', ' + operation.machine + ').\n\n')
         
-f.write('starts(pseudo, 0, 0).\n lasts(pseudo, 0).\n')
-f.write('starts(J, M, T)  :- dependson(J, A), endsbefore(A, T), runson(J, M), isfree(M, T).\n'
-        'endsbefore(J, T) :- starts(J, _, Z), Z + W < T, lasts(J, W).\n'
-        'isfree(M, T)     :- starts(J, M, Z), Z < T, lasts(J, W), Z + W <= T.\n'
-        'endsat(J, T)     :- T = #sum { Z, W : starts(J, _, Z), lasts(J, W) }.\n'
-        'max(X)           :- X = #max { T : endsat(J, T) }.\n'
-        '#minimize { X : max(X) }.\n'
-       )
-f.close()
+for machine in machines:
+    f.write('machine(' + machine + ').\n')
+f.write('\n')
 
+f.write('%' + str(len(operations)) + '{starts(O, M, T) : operation(O), machine(M), T = 1..' + str(max_sum) + '}'+ str(len(operations)) +'.\n'
+        '%:- #count {J : starts(J, _, _)} != ' + str(len(operations)) + ', operation(J).\n'
+        '%:- starts(J1, _, _), starts(J2, _, _), J1 = J2.\n'
+        '' + str(len(operations)) + '{starts(J, M, T) : '
+        'dependson(J, A), endsbefore(J, A), runson(J, M), isfree(M, T), operation(J), operation(A), machine(M)}'+ str(len(operations)) + '.\n'
+        'endsbefore(J, A)     :- starts(J, _, Z), endsat(A, T), Z < T, operation(J), operation(A).\n'
+        'isfree(M, T + W + 1) :- starts(J, M, T), lasts(J, W), operation(J), machine(M).\n'
+        '%isfree(M, T + 1)    :- not starts(_ , M, T+1), isfree(M, T).\n'
+        'endsat(J, T + W)     :- starts(J, _, T), lasts(J, W), operation(J).\n'
+        'max(S)               :- S = #max { T : endsat(_, T) }.\n'
+        '%#minimize { V : max(V) }.\n'
+        '%#show starts/3.'
+       )
+
+f.close()
 
 os.system('clingo 0 mysolution.lp')
